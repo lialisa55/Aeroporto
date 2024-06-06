9ba0db3fee17b5b72f251dde19d77e3940c77974
chiroderma <jvamello@icloud.com>
2024-06-06 14:25:09 -0300

    Update hamburguer.c

--- hamburguer.c
+++ hamburguer.c
@@ -1,2 +1,9 @@
 //fcsadfasfasfas
 #include <stdio.h>
+struct Passageiro
+{
+    char* nome, sobrenome, cpf, assento, classe, origem, destino;
+    float valor;
+    int data[3], numero_voo;
+}
+typedef struct Passageiro passageiro;
