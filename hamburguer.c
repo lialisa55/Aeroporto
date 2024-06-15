168b8aa051ada81dfa4375ca95cc7583de99f8c6
chiroderma <jvamello@icloud.com>
2024-06-14 21:16:00 -0300

    Update hamburguer.c

--- hamburguer.c
+++ hamburguer.c
@@ -18,12 +18,11 @@ char *alocar_string(int x);
 passageiro *realocar_passageiros(passageiro *p, int x);
 char *enxutarString(char *a);
 passageiro registrar_passageiro();
-void fechamentoVoo(float informacoes_do_voo[4], int tamanho_lista_passageiros, passageiro *lista_passageiros);
+void fechamentoVoo(FILE *fp, float informacoes_do_voo[4], int tamanho_lista_passageiros, passageiro *lista_passageiros);
 void imprimirPassageiro(passageiro pessoa, char *modo);
-void f_imprimirPassageiro(FILE *fp, passageiro pessoa);
 int acharCPF(char * cpf, passageiro *lista_passageiros, int tamanho_lista_passageiros);
-void salvarDados(FILE *fp, float informacoes_do_voo[4], int tamanho_lista_passageiros, passageiro *lista_passageiros);
-void lerDadosSalvos(FILE *fp, float informacoes_do_voo[4], int *tamanho_lista_passageiros, passageiro **lista_passageiros);
+void salvarDados(FILE *fp, int fechado, float informacoes_do_voo[4], int tamanho_lista_passageiros, passageiro *lista_passageiros);
+int lerDadosSalvos(FILE *fp, float informacoes_do_voo[4], int *tamanho_lista_passageiros, passageiro **lista_passageiros);
 
 char *alocar_string(int x){
     char *b = malloc(x * sizeof(char));
@@ -35,6 +34,10 @@ char *alocar_string(int x){
 }
 
 passageiro *realocar_passageiros(passageiro *p, int x){
+    if (x == 0){
+        free(p);
+        return NULL;
+    }
     passageiro *b = realloc(p, x * sizeof(passageiro));
     if (b == NULL){
         printf("oops! hehe nao tenho memoria o suficiente senpai ;*_*;");
@@ -43,22 +46,8 @@ passageiro *realocar_passageiros(passageiro *p, int x){
     return b;
 }
 
-/*chamado pelo comando AV*/
-void registrar_novo_voo(float informacoes_do_voo[3]){
-    scanf("%f %f %f", &informacoes_do_voo[0], &informacoes_do_voo[1], &informacoes_do_voo[2]);
-    //insira jeito de registrar num arquivo
-}
-
-char *enxutarString(char *a){
-    char *b = realloc(a, (strlen(a) + 1) * sizeof(char));
-    if (b == NULL){
-        printf("OWO sumimasen senpai-san! nao tenho memoria o suficiente UWU");
-        exit(1);
-    }
-    return b;
-}
 /*chamado pelo comando RR*/
-passageiro registrar_passageiro(){ //se fp for NULL, use scanf normal
+passageiro registrar_passageiro(){
     passageiro persona;
     scanf("%s %s %s %d %d %d %s %s %s %f %s %s",
     persona.nome, persona.sobrenome, persona.cpf, &persona.data[0], &persona.data[1], &persona.data[2], persona.numero_voo, persona.assento, persona.classe, &persona.valor, persona.origem, persona.destino);
@@ -66,10 +55,10 @@ passageiro registrar_passageiro(){ //se fp for NULL, use scanf normal
 }
 
 /*chamado pelo comando FV*/
-void fechamentoVoo(float informacoes_do_voo[4], int tamanho_lista_passageiros, passageiro *lista_passageiros) {
+void fechamentoVoo(FILE *fp, float informacoes_do_voo[4], int tamanho_lista_passageiros, passageiro *lista_passageiros) {
+    printf("Voo Fechado!\n\n");
     //qtdPassageiros é obtido por uma váriavel que é incrimentada quando chama o comando RR;
-    FILE *fp = fopen("dados.txt", "wb");
-    salvarDados(fp, informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
+    salvarDados(fp, 1, informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
     //Ainda não sei como achar essa parte específica no arquivo;
     //MAS EU SEI KKKKKKKKKKKK
     
@@ -80,7 +69,6 @@ void fechamentoVoo(float informacoes_do_voo[4], int tamanho_lista_passageiros, p
     }
     printf("Valor Total: %.2f\n", informacoes_do_voo[3]);
     printf("--------------------------------------------------");
-    fclose(fp);
 }
 
 void imprimirPassageiro(passageiro pessoa, char *modo){
@@ -101,17 +89,6 @@ void imprimirPassageiro(passageiro pessoa, char *modo){
     }
 }
 
-void f_imprimirPassageiro(FILE *fp, passageiro pessoa){
-    fprintf(fp, "%s %s ", pessoa.nome, pessoa.sobrenome);
-    fprintf(fp, "%s ", pessoa.cpf);
-    fprintf(fp, "%d %d %d ", pessoa.data[0], pessoa.data[1], pessoa.data[2]);
-    fprintf(fp, "%s ", pessoa.numero_voo);
-    fprintf(fp, "%s ", pessoa.assento);
-    fprintf(fp, "%s ", pessoa.classe);
-    fprintf(fp, "%.02f ", pessoa.valor);
-    fprintf(fp, "%s %s ", pessoa.origem, pessoa.destino);
-}
-
 int acharCPF(char * cpf, passageiro *lista_passageiros, int tamanho_lista_passageiros){
     for (int i = 0; i < tamanho_lista_passageiros; i++){
         if (!strcmp(lista_passageiros[i].cpf, cpf)){
@@ -121,17 +98,21 @@ int acharCPF(char * cpf, passageiro *lista_passageiros, int tamanho_lista_passag
     return -1;
 }
 
-void salvarDados(FILE *fp, float informacoes_do_voo[4], int tamanho_lista_passageiros, passageiro *lista_passageiros){
+void salvarDados(FILE *fp, int fechado, float informacoes_do_voo[4], int tamanho_lista_passageiros, passageiro *lista_passageiros){
+    fwrite(&fechado, sizeof(int), 1, fp);
     fwrite(informacoes_do_voo, sizeof(float), 4, fp);
     fwrite(&tamanho_lista_passageiros, sizeof(int), 1, fp);
     fwrite(lista_passageiros, sizeof(passageiro), tamanho_lista_passageiros, fp);
 }
 
-void lerDadosSalvos(FILE *fp, float informacoes_do_voo[4], int *tamanho_lista_passageiros, passageiro **lista_passageiros){
+int lerDadosSalvos(FILE *fp, float informacoes_do_voo[4], int *tamanho_lista_passageiros, passageiro **lista_passageiros){
+    int tmp;
+    fread(&tmp, sizeof(int), 1, fp);
     fread(informacoes_do_voo, sizeof(float), 4, fp);
     fread(tamanho_lista_passageiros, sizeof(int), 1, fp);
     *lista_passageiros = realocar_passageiros(*lista_passageiros, *tamanho_lista_passageiros);
     fread(*lista_passageiros, sizeof(passageiro), *tamanho_lista_passageiros, fp);
+    return tmp;
 }
 
 
@@ -142,22 +123,23 @@ void deletarCPF(int indice, passageiro **lista_passageiros, int *tamanho_lista_p
     }
     informacoes_do_voo[3] -= (*lista_passageiros)[indice].valor;
     (*tamanho_lista_passageiros)--;
-    (*lista_passageiros)[indice] = (*lista_passageiros)[*tamanho_lista_passageiros];
-    if (*tamanho_lista_passageiros > 1) *lista_passageiros = realocar_passageiros(*lista_passageiros, *tamanho_lista_passageiros);
-    else{
-        free(*lista_passageiros);
-        *lista_passageiros = NULL;
-    }
+    for (int i = indice; i < *tamanho_lista_passageiros; i++) (*lista_passageiros)[i] = (*lista_passageiros)[i + 1];
+    *lista_passageiros = realocar_passageiros(*lista_passageiros, *tamanho_lista_passageiros);
 }
 
 int main (void){
     float informacoes_do_voo[4] = {0};
     passageiro *lista_passageiros = NULL;
     int tamanho_lista_passageiros = 0;
-    FILE *fp = fopen("dados.txt", "rb");
+    FILE *fp = fopen("dados.bin", "rb");
     if (fp != NULL){ //le dados salvos anteriormente (caso nao esteja claro o suficiente pra voce :)))))))))))))))))))))))))
         printf("arquivo encontrado!\n");
-        lerDadosSalvos(fp, informacoes_do_voo, &tamanho_lista_passageiros, &lista_passageiros);
+        if (lerDadosSalvos(fp, informacoes_do_voo, &tamanho_lista_passageiros, &lista_passageiros)){
+            fechamentoVoo(fp, informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
+            fclose(fp);
+            free(lista_passageiros);
+            return 0;
+        };
         fclose(fp);
     }
     char comando[3] = "NA";
@@ -173,7 +155,9 @@ int main (void){
             informacoes_do_voo[3] += lista_passageiros[tamanho_lista_passageiros - 1].valor;
             if (tamanho_lista_passageiros == informacoes_do_voo[0]){
                 strcpy(comando, "FV");
-                fechamentoVoo(informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
+                FILE *fp = fopen("dados.bin", "wb");
+                fechamentoVoo(fp, informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
+                fclose(fp);
             }
         }
         else if (!strcmp(comando, "MR")){
@@ -185,6 +169,9 @@ int main (void){
                 continue;
             }
             scanf("%s %s %s %s", lista_passageiros[indice].nome, lista_passageiros[indice].sobrenome, lista_passageiros[indice].cpf, lista_passageiros[indice].assento);
+            printf("Reserva Modificada:\n");
+            imprimirPassageiro(lista_passageiros[indice], "longo");
+            printf("--------------------------------------------------\n");
         }
         else if (!strcmp(comando, "CR")){ //encontra um cpf e imprime as informacoes de seu dono
             char cpf[100];
@@ -201,23 +188,29 @@ int main (void){
             deletarCPF(indice, &lista_passageiros, &tamanho_lista_passageiros, informacoes_do_voo);
         }
         else if (!strcmp(comando, "FD")){ //encerra o programa e salva as informacoes obtidas
-            FILE *fp = fopen("dados.txt", "wb");
+            FILE *fp = fopen("dados.bin", "wb");
             if (fp == NULL) printf("FP ta nulo!\n");
             printf("Fechamento do dia:\n");
             printf("Quantidade de reservas: %d\n", tamanho_lista_passageiros);
             printf("Posição: %f\n", informacoes_do_voo[3]);
             printf("--------------------------------------------------");
-            salvarDados(fp, informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
+            salvarDados(fp, 0, informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
             fclose(fp);
         }
         else if (!strcmp(comando, "FV")){
-            fechamentoVoo(informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
+            FILE *fp = fopen("dados.bin", "wb");
+            fechamentoVoo(fp, informacoes_do_voo, tamanho_lista_passageiros, lista_passageiros);
+            fclose(fp);
         }
     }
-    /*FILE *fp;
-    fp = fopen("file.txt", "w+");
-    char *instrucao;
-    char *cpf[20];
-
-    consultarcpf(cpf, fp);*/
+    if (lista_passageiros != NULL) free(lista_passageiros);
 }
+/*AV 200 1200.00 2500.00
+RR Carlos Massa 555.555.333-99 12 12 2024
+V001 A27 economica 1200.00 CGH RAO
+RR Maria Massa 444.555.333-93 12 12 2024
+V001 A31 economica 1200.00 CGH RAO
+RR Roberto Carlos 555.333.333-89 12 12 2024
+V001 P12 executiva 2500.00 CGH RAO
+MR 555.555.333-99 Carlos Massa 555.555.333-99 A30
+FD*/
